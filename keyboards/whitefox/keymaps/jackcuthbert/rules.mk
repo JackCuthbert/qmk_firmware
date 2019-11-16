7547c9d32f552935123a06afff4960f0299ece5d
Jack Cuthbert <jckcthbrt@gmail.com>
2019-11-17 09:24:32 +1100

    ALWAYS LATEST, REBASE UPSTREAM (Add personal layouts)

---
+++ keyboards/whitefox/keymaps/jackcuthbert/rules.mk
@@ -0,0 +1,11 @@
+# Generic features
+NKRO_ENABLE        = yes
+
+# Keyboard-specific features
+BACKLIGHT_ENABLE   = no
+VISUALIZER_ENABLE  = no
+
+# Firmware size reduction
+GRAVE_ESC_ENABLE   = no
+MAGIC_ENABLE       = no
+SPACE_CADET_ENABLE = no
